b1b07c2e15966f94bbe7e7633d5476c270d3f1ab
Jasper Makkinje <jaspermakkinje@gmail.com>
2021-05-04 20:01:53 +0100

    Add type hinting and logging

--- cbbi_bot/handler.py
+++ cbbi_bot/handler.py
@@ -1,16 +1,21 @@
 import telegram
-from constants import TOKEN, PRIVATE_CHAT_ID, CHANNEL_CHAT_ID
-from functions import get_cbbi_data, format_telegram_message
+from constants import CHANNEL_CHAT_ID, PRIVATE_CHAT_ID, TOKEN
+from functions import format_telegram_message, get_cbbi_data
+from loguru import logger
 
 
-def send_message(event=None, context=None):
+def send_message(event=None, context=None) -> None:
+    logger.info("Sending daily CBBI update!")
     data = get_cbbi_data()
     message = format_telegram_message(data)
-
     bot = telegram.Bot(token=TOKEN)
-    bot.sendMessage(chat_id=PRIVATE_CHAT_ID, text=message, parse_mode='MarkdownV2')
-    bot.sendMessage(chat_id=CHANNEL_CHAT_ID, text=message, parse_mode='MarkdownV2')
+
+    logger.info("Sending message to group...")
+    bot.sendMessage(chat_id=PRIVATE_CHAT_ID, text=message, parse_mode="MarkdownV2")
+
+    logger.info("Sending message to channel...")
+    bot.sendMessage(chat_id=CHANNEL_CHAT_ID, text=message, parse_mode="MarkdownV2")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     send_message()
